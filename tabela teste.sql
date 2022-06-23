c763a78d0aeb30cf3058992e9dd22e56961b5777
Tales Lima <tales_lima_1982@hotmail.com>
2022-06-23 16:06:48 +0200

    cadastro test

--- tabela teste.sql
+++ tabela teste.sql
@@ -1,6 +1,10 @@
-insert into `pessoas`
-(`id`,`nome`,`nacimento`,`sexo`,`peso`,`altura`,`nacionalidade`)
-values
-(default,'Marlete','1965-04-17','F','65','1.62','default');
-
-select * from `pessoas`;
+create table pessoas(
+id int not null auto_increment,
+nome varchar(30) not null,
+nacimento date,
+sexo enum('M','F'),
+peso decimal(5,2),
+altura decimal (3,2),
+nacionalidade varchar(20) default 'Brasil',
+primary key(id)
+)default char set = utf8;
