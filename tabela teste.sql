24e549e7271786b8225aeabf5a33f2cbd108398e
Tales Lima <tales_lima_1982@hotmail.com>
2022-06-23 13:38:44 +0200

    update

---
+++ tabela teste.sql
@@ -0,0 +1,6 @@
+insert into `pessoas`
+(`id`,`nome`,`nacimento`,`sexo`,`peso`,`altura`,`nacionalidade`)
+values
+(default,'Marlete','1965-04-17','F','65','1.62','default');
+
+select * from `pessoas`;
