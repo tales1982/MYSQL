7f6a765c55113e3c4ac8c0d2782e288710ea5656
Tales Lima <tales_lima_1982@hotmail.com>
2022-06-23 16:15:27 +0200

    update

---
+++ cadastro.sql
@@ -0,0 +1,7 @@
+insert into pessoas
+(nome, nacimento, sexo, peso, altura, nacionalidade)
+values
+('Suelia','1983-10-15','F','65','1.52','Brasil'),
+('Joao','1987-01-05','M','85','1.72',default),
+('paulo','1984-12-15','M','87','1.92','USA');
+select * from pessoas;
